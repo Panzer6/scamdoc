0ec94ca8d8e4e8237cfa59873aa904ded3400796
Panzer6 <60883330+Panzer6@users.noreply.github.com>
2023-02-01 10:42:16 +0800

    Add files via upload
    - added new requirements to pipinstall

--- pipinstall.bat
+++ pipinstall.bat
@@ -1,11 +1,11 @@
 @echo off
 
-
+winget install --id Git.Git -e --source winget
 pip install undetected-chromedriver
 pip install datetime
 pip install bs4
 pip install lxml
-pip install selenium
+pip install git+https://github.com/lukect/undetected-chromedriver.git selenium~=4.8.0
 
 
 pause
