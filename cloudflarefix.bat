daac5cad62168873532152b4e5660abdb3c93181
Panzer6 <60883330+Panzer6@users.noreply.github.com>
2023-02-01 10:49:59 +0800

    Add files via upload

---
+++ cloudflarefix.bat
@@ -0,0 +1,6 @@
+@echo off
+
+pip install git+https://github.com/lukect/undetected-chromedriver.git selenium~=4.8.0
+
+pause
+exit
--- pipinstall.bat
+++ pipinstall.bat
@@ -5,8 +5,6 @@ pip install undetected-chromedriver
 pip install datetime
 pip install bs4
 pip install lxml
-pip install git+https://github.com/lukect/undetected-chromedriver.git selenium~=4.8.0
-
 
 pause
 exit
